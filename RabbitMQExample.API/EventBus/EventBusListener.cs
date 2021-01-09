eec22e125830145a70f3eda056cbe69dc90f4417
SJVarela <s.javiervarela@gmail.com>
2021-01-09 10:53:17 -0300

    Add project files.

---
+++ RabbitMQExample.API/EventBus/EventBusListener.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQExample.API.EventBus
+{
+    public class EventBusListener : IHostedService
+    {
+        private IModel _channel;
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "host.docker.internal", Port = 999 };
+            var connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+            _channel.ExchangeDeclare("ApiExchange", ExchangeType.Fanout);
+            _channel.QueueDeclare(queue: "APIQueue");
+            _channel.QueueBind("APIQueue", "ApiExchange", "");
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += Consumer_Received;
+            _channel.BasicConsume("APIQueue", true, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _channel.Dispose();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RabbitMQExample.API/EventBus/EventBusPublisher.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQExample.API.EventBus
+{
+    public class EventBusPublisher
+    {
+        public void PublishEvent(object message)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "host.docker.internal", Port = 999 };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var serializedMessage = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(serializedMessage);
+
+            channel.BasicPublish(exchange: "ApiExchange", body: body, routingKey: "");
+        }
+    }
+}
